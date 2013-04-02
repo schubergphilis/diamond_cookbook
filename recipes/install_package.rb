492e21838553b5014b74cedfe04a7402d2d841c7
Roeland Kuipers <rkuipers@schubergphilis.com>
2013-04-02 10:57:16 +0200

    fix attributes for TCP and add rpm and deb packages to cookbook

--- recipes/install_package.rb
+++ recipes/install_package.rb
@@ -4,16 +4,29 @@ case node[:platform]
       action :install
     end
 
+    cookbook_file "diamond_3.3.352_all.deb" do
+        backup false
+        path "/tmp/diamond_3.3.deb"
+        action :create_if_missing
+    end
     package "diamond" do
-      action :install
-      version node['diamond']['version']
-      notifies :restart, resources(:service => "diamond")
+        source "/tmp/diamond_3.3.deb"
+        action :install
+        provider Chef::Provider::Package::Dpkg
+        notifies :restart, resources(:service => "diamond")
     end
 
+
   when "centos", "redhat", "fedora", "amazon", "scientific"
+   
+    cookbook_file "diamond-3.3.352-0.noarch.rpm" do
+        backup false
+        path "/tmp/diamond_3.3.rpm"
+        action :create_if_missing
+    end
     package "diamond" do
-      action :install
-      version node['diamond']['version']
-      notifies :restart, resources(:service => "diamond")
+        source "/tmp/diamond_3.3.rpm"
+        action :install
+        notifies :restart, resources(:service => "diamond")
     end
 end
