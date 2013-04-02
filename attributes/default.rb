10a4fb562e9c0df47cc9d9c41ab0e227b81b1d99
Roeland Kuipers <rkuipers@schubergphilis.com>
2013-04-02 09:45:54 +0200

    merge

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,7 @@
-default["diamond"]["install_method"] = "package"
-default["diamond"]["graphite_server_role"] = nil
-default["diamond"]["graphite_server"] = "graphite"
-default["diamond"]["handlers"] = "diamond.handler.graphite.GraphiteHandler, diamond.handler.archive.ArchiveHandler"
 case node[:platform]
   when "ubuntu","debian"
     default[:diamond][:version] = '3.0.2'
   else
     default[:diamond][:version] = '3.0.2-0'
 end
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,33 +4,47 @@ service "diamond" do
   action [ :nothing ]
 end
 
-include_recipe "diamond::install_%s" % [node['diamond']['install_method']]
+case node[:platform]
+  when "debian", "ubuntu"
+    package "python-pysnmp4" do
+      action :install
+    end
+
+  cookbook_file "diamond_3.3.352_all.deb" do
+    backup false
+    path "/tmp/diamond_3.3.deb"
+    action :create_if_missing
+  end
+    package "diamond" do
+      source "/tmp/diamond_3.3.deb"	
+      action :install
+      provider Chef::Provider::Package::Dpkg
+      notifies :restart, resources(:service => "diamond")
+    end
+
+  when "centos", "redhat", "fedora", "amazon", "scientific"
+    cookbook_file "diamond-3.3.352-0.noarch.rpm" do
+    backup false
+    path "/tmp/diamond_3.3.rpm"
+    action :create_if_missing
+   end
+    package "diamond" do
+      source "/tmp/diamond_3.3.rpm"
+      action :install
+      notifies :restart, resources(:service => "diamond")
+    end
+end
 
 service "diamond" do
   action [ :enable ]
 end
 
-if node['diamond']['graphite_server_role'].nil?
-  graphite_ip = node['diamond']['graphite_server']
-else
-  graphite_nodes = search(:node, "role:%s" % [node['diamond']['graphite_server_role']])
-  if graphite_nodes.empty?
-    Chef::Log.warn("No nodes returned from search")
-    graphite_ip = node['diamond']['graphite_server']
-  else
-    graphite_ip = graphite_nodes[0]["ipaddress"]
-  end
-end
-
-template "/etc/diamond/diamond.conf" do
-  source "diamond.conf.erb"
+cookbook_file "/etc/diamond/diamond.conf" do
+  source "diamond.conf"
   owner "root"
   group "root"
   mode "0644"
   notifies :restart, resources(:service => "diamond")
-  variables(
-    :graphite_ip => graphite_ip
-  )
 end
 
 #install basic collector configs
